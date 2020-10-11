65f705cc46bed201cc0a3ae58f043d604d7284c8
maikytech <maikytech1307@gmail.com>
2020-10-11 15:19:17 -0500

    Start Project

---
+++ Tweet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tweet
+//
+//  Created by Maiqui Cedeño on 11/10/20.
+//  Copyright © 2020 Poseto Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
