df3557abe0940d4206455e7ec72007221ee6d131
maikytech <maikytech1307@gmail.com>
2020-10-16 18:39:11 -0500

    [Add] Welcome, Login and Register ViewControllers

---
+++ Tweet/Commons/Welcome/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Tweet
+//
+//  Created by Maiqui Cedeño on 16/10/20.
+//  Copyright © 2020 Poseto Studio. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
