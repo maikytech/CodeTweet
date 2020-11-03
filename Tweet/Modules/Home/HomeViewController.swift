25792350e56c3f1357af512028dabd5188470395
maikytech <maikytech1307@gmail.com>
2020-11-02 19:06:18 -0500

    [Update] HomeViewController cell configuration

--- Tweet/Modules/Home/HomeViewController.swift
+++ Tweet/Modules/Home/HomeViewController.swift
@@ -13,9 +13,40 @@ class HomeViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet weak var tableView: UITableView!
     
+    //MARK: - Properties
+    private let cellId = "TweetTableViewCell"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
 
+    }
+    
+    private func setupUI() {
+        
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: cellId, bundle: nil), forCellReuseIdentifier: cellId)
+    }
+}
+
+//MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        if let cell = cell as? TweetTableViewCell {
+            
+            //Cell configuration
+        }
+        return cell
     }
 }
