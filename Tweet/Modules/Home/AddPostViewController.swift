ea81b81d1370e025b6c686ddbf6f84e71dffc34d
maikytech <maikytech1307@gmail.com>
2020-11-03 21:51:15 -0500

    [Add] New tweet viewController

---
+++ Tweet/Modules/Home/AddPostViewController.swift
@@ -0,0 +1,33 @@
+//
+//  AddPostViewController.swift
+//  Tweet
+//
+//  Created by Maiqui Cedeño on 3/11/20.
+//  Copyright © 2020 Poseto Studio. All rights reserved.
+//
+
+import UIKit
+
+class AddPostViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var postTextView: UITextView!
+    
+    //MARK: - IBActions
+    @IBAction func addPostAction() {
+        
+        
+    }
+    
+    @IBAction func dismissAction() {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
